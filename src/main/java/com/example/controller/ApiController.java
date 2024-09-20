9a8287900bbf866be3826b0c0f96655057fbb56a
Juan Grela <j.grela@sieteideas.com.ar>
2024-09-20 15:58:41 -0300

    Primer servicio de prueba

---
+++ src/main/java/com/example/controller/ApiController.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+	@GetMapping("/test")
+	public String test() {
+		return "Hola Mundo!";
+	}
+	
+	
+	
+}
