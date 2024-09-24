1dd6b4cd050735bc8efd9182bea7f853b7fc638a
Juan Grela <j.grela@sieteideas.com.ar>
2024-09-24 16:40:55 -0300

    Agrego servicio para obtener informacion del proyecto

--- src/main/java/com/example/controller/ApiController.java
+++ src/main/java/com/example/controller/ApiController.java
@@ -1,16 +1,28 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
+	@Autowired
+	BuildProperties buildProperties;
+	
+	
 	@GetMapping("/test")
 	public String test() {
 		return "Hola Mundo!";
 	}
 	
-	
+    @GetMapping("/project-info")
+    public String getProjectInfo() {
+    	return "Nombre del proyecto: " + buildProperties.getName() + 
+    			"\nVersion de proyecto: " + buildProperties.getVersion() + 
+    			"\nVersion de Java: " + buildProperties.get("java.version") +
+    			"\nVersion de Spring Boot: " + buildProperties.get("springboot.version");
+    }
 	
 }
